883d7fbb81039e9dc502082da3f09912428b8c48
Johvahn <johvahnbro@gmail.com>
2021-10-07 17:29:26 -0500

    Edited

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,26 @@
-h
+window.addEventListener('DOMContentLoaded' , () => {
+    var children= document.getElementById('board').children;
+    for(let child=0; child < children.length; child++){
+        children[child].setAttribute('class', 'square');
+        children[child].addEventListener('click', (o) => {
+            children[child].textContent = 'o'
+            
+        });
+        children[child].addEventListener('click', (x) => {
+            children[child].textContent = 'x'
+            
+            
+        });
+        
+         
+
+       
+        };
+        
+        
+        
+       
+            
+    
+        btnButton.addEventlistener('click', resetboard);
+});
