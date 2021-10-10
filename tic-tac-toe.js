227826e9c8e6f30dae92394456ef40dbf8072df1
Johvahn <johvahnbro@gmail.com>
2021-10-09 22:14:59 -0500

    Edited

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -37,41 +37,49 @@ for(var s = 0; s < boardSquares.length; s++){
             var jackpot = boardSquares[0].innerHTML;
             document.getElementById("status").innerHTML = "Congratulations! " + jackpot + " is the Winner!";
             document.getElementById("status").className=("you-won");
+            for( const btn of boardSquares){btn.style.pointerEvents = 'none';}
         }else if (boardSquares[0].innerHTML === "X" && boardSquares[0].innerHTML === boardSquares[3].innerHTML && boardSquares[0].innerHTML === boardSquares[6].innerHTML ||
         boardSquares[0].innerHTML === "O" && boardSquares[0].innerHTML === boardSquares[3].innerHTML && boardSquares[0].innerHTML === boardSquares[6].innerHTML ){
             var jackpot = boardSquares[0].innerHTML;
             document.getElementById("status").innerHTML = "Congratulations! " + jackpot + " is the Winner!";
             document.getElementById("status").className=("you-won");
+            for( const btn of boardSquares){btn.style.pointerEvents = 'none';}
         }else if (boardSquares[0].innerHTML === 'X' && boardSquares[0].innerHTML === boardSquares[4].innerHTML && boardSquares[0].innerHTML === boardSquares[8].innerHTML ||
         boardSquares[0].innerHTML !== "O" && boardSquares[0].innerHTML === boardSquares[4].innerHTML && boardSquares[0].innerHTML === boardSquares[8].innerHTML){
             var jackpot = boardSquares[0].innerHTML;
             document.getElementById("status").innerHTML = "Congratulations! " + jackpot + " is the Winner!";
             document.getElementById("status").className=("you-won");
+            for( const btn of boardSquares){btn.style.pointerEvents = 'none';}
         }else if (boardSquares[3].innerHTML === "X" && boardSquares[3].innerHTML === boardSquares[4].innerHTML && boardSquares[3].innerHTML === boardSquares[5].innerHTML ||
         boardSquares[3].innerHTML === "O" && boardSquares[3].innerHTML === boardSquares[4].innerHTML && boardSquares[3].innerHTML === boardSquares[5].innerHTML ){
             var jackpot = boardSquares[3].innerHTML;
             document.getElementById("status").innerHTML = "Congratulations! " + jackpot + " is the Winner!";
             document.getElementById("status").className=("you-won");
+            for( const btn of boardSquares){btn.style.pointerEvents = 'none';}
         }else if (boardSquares[6].innerHTML === "X" && boardSquares[6].innerHTML === boardSquares[7].innerHTML && boardSquares[6].innerHTML === boardSquares[8].innerHTML ||
         boardSquares[6].innerHTML === "O" && boardSquares[6].innerHTML === boardSquares[7].innerHTML && boardSquares[6].innerHTML === boardSquares[8].innerHTML ){
             var jackpot = boardSquares[6].innerHTML;
             document.getElementById("status").innerHTML = "Congratulations! " + jackpot + " is the Winner!";
             document.getElementById("status").className=("you-won");
+            for( const btn of boardSquares){btn.style.pointerEvents = 'none';}
         }else if (boardSquares[1].innerHTML === "X" && boardSquares[1].innerHTML === boardSquares[4].innerHTML && boardSquares[1].innerHTML === boardSquares[7].innerHTML ||
         boardSquares[1].innerHTML === "O" && boardSquares[1].innerHTML === boardSquares[4].innerHTML && boardSquares[1].innerHTML === boardSquares[7].innerHTML){
             var jackpot = boardSquares[1].innerHTML;
             document.getElementById("status").innerHTML = "Congratulations! " + jackpot + " is the Winner!";
             document.getElementById("status").className=("you-won");
+            for( const btn of boardSquares){btn.style.pointerEvents = 'none';}
         }else if (boardSquares[2].innerHTML === "X" && boardSquares[2].innerHTML === boardSquares[5].innerHTML && boardSquares[2].innerHTML === boardSquares[8].innerHTML ||
         boardSquares[2].innerHTML === "O" && boardSquares[2].innerHTML === boardSquares[5].innerHTML && boardSquares[2].innerHTML === boardSquares[8].innerHTML ){
             var jackpot = boardSquares[2].innerHTML;
             document.getElementById("status").innerHTML = "Congratulations! " + jackpot + " is the Winner!";
             document.getElementById("status").className=("you-won");
+            for( const btn of boardSquares){btn.style.pointerEvents = 'none';}
         }else if (boardSquares[2].innerHTML === "X" && boardSquares[4].innerHTML === 'X' && boardSquares[6].innerHTML === 'X'  ||
         boardSquares[2].innerHTML === "O" && boardSquares[4].innerHTML === 'O' && boardSquares[6].innerHTML === "O"  ){
             var jackpot = boardSquares[2].innerHTML;
             document.getElementById("status").innerHTML = "Congratulations! " + jackpot + " is the Winner!";
             document.getElementById("status").className=("you-won");
+            for( const btn of boardSquares){btn.style.pointerEvents = 'none';}
         }
     }
     var restartbtn = document.querySelector("button");
